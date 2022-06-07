c6be6ba19cf4ad1788347c7aff900fc0219d62ee
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-06-07 08:26:49 -0400

    added cards for days

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
-var travelDates = $(".travel-dates")
+// var travelDates = $(".travel-dates")
 
-for (let day = 1; day <= 7; day++){
+// for (let day = 1; day <= 7; day++){
 
     
-    console.log(day);
-    travelDates.append(`<div 
-    class= "day">${day}</div>`)
+//     console.log(day);
+//     travelDates.append(`<div 
+//     class= "day">${day}</div>`)
 
-}
+// }
 
