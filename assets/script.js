597b9466605ec0dd4f8771f9c5749b3c73de1a34
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-06-06 21:25:29 -0400

    console log days

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,7 @@ var travelDates = $(".travel-dates")
 
 for (let day = 1; day <= 7; day++){
 
-    travelDates.insertAdjacentHTML("beforeend", `<div 
-    class= "day">${day}</div>`);
+    
     console.log(day);
 
 }
