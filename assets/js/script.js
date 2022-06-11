ad5ef3de3db449b7171a22428f497cb6019d0d19
Michael Sullivan <104106828+ms13380@users.noreply.github.com>
2022-06-11 11:45:37 -0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,7 @@ var secondDay = $("#2")
 var thirdDay = $("#3")
 var forthDay = $("#4")
 var fifthDay = $("#5")
-
 var today = moment()
-
-//Store address to localStorage
-var tripdata = []
-localStorage.setItem("TripKey", tripdata)
-    const trip = {
-        StartAddress: "",
-        EndAddress: "",
-        CityName: "",
-        Date: ""
-    }
-    window.localStorage.setItem('tripdata',JSON.stringify(tripdata))
-var tripdata = localStorage.getItem("TripKey")
 
 currentDay.text(today.format("ddd MMMM Do"))
 console.log(currentDay);
@@ -34,5 +21,39 @@ console.log(forthDay)
 
 fifthDay.text(today.add(1, 'days').format("ddd MMMM Do"));
 console.log(fifthDay)
+
+//Stores items in localStorage
+function store() {
+    var StartAddress = document.getElementById('Start').value;
+    var EndAddress = document.getElementById('End').value;
+    var CityName = document.getElementById('City').value;
+    var Date = document.getElementById('Date').value;
+    
+var tripdata = []
+
+//Set data to localStorage
+localStorage.setItem("TripKey", tripdata)
+    const trip = {
+        StartAddress: "",
+        EndAddress: "",
+        CityName: "",
+        Date: ""
+    }
+    //stringify object
+    window.localStorage.setItem('tripdata',JSON.stringify(tripdata))
+}
+
+//pull from localStorage
+function retrievetripdata(){
+    var retrievekey = document.getElementById('retrievekey').value;
+    console.log("Retrieve Trip Data")
+}
+
+//Loads page before functions execute
+window.onload = function(){
+    document.getElementById("submitBtn").onsubmit = store
+    document.getElementById("NEED BUTTON NAME").onclick = retrievetripdata
+}
+
 
 
