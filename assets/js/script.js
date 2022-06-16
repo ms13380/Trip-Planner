0f09ae03feedd0c23b8d9fc69da278965d38acc5
BC_IML <lelj28819@gmail.com>
2022-06-16 13:32:23 -0400

    Adding Local Storage for places visited, fix bug for comma in destination city for the weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
+var endAddress = $('#destCity').val()
+var startAddress = $('#startCity').val()
+var submitBtn = $('#submitBtn')
+var cityName = endAddress.split(",")[0];
+var  date 
 var currentDay = $("#1")
-
 // Expanded Forecast
 var secondDay = $("#2")
 var thirdDay = $("#3")
@@ -9,7 +13,7 @@ var today = moment()
 
 currentDay.text(today.format("ddd MMMM Do"))
 console.log(currentDay);
-
+date = currentDay
 secondDay.text(today.add(1, 'days').format("ddd MMMM Do"));
 console.log(secondDay)
 
@@ -22,8 +26,13 @@ console.log(forthDay)
 fifthDay.text(today.add(1, 'days').format("ddd MMMM Do"));
 console.log(fifthDay)
 
+// Store the information after submit
+submitBtn.click(store(startAddress, endAddress, cityName, date  ));
+
+
+
 //Stores items in localStorage
-function store() {
+function store(startAddress, endAddress, cityName, date  ) {
     var StartAddress = document.getElementById('from-input').value;
     var EndAddress = document.getElementById('too-input').value;
     var CityName = document.getElementById('destCity').value;
