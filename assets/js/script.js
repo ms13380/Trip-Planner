d40675196d5dd4375fb64f4fad11abd83b102728
BC_IML <lelj28819@gmail.com>
2022-06-17 19:51:34 -0500

    Remove decimals for temperature, remove console.logs, remove data picker test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,4 @@
-var endAddress = $('#destCity').val()
-var startAddress = $('#startCity').val()
-var submitBtn = $('#submitBtn')
-var cityName = endAddress.split(",")[0];
-var  date 
+
 
 // Dates for Cards - Moment.js
 var currentDay = $("#1")
@@ -14,31 +10,23 @@ var fifthDay = $("#5")
 var today = moment()
 
 currentDay.text(today.format("ddd MMMM Do"))
-console.log(currentDay);
+
 date = currentDay
 secondDay.text(today.add(1, 'days').format("ddd MMMM Do"));
-console.log(secondDay)
-
 thirdDay.text(today.add(1, 'days').format("ddd MMMM Do"));
-console.log(thirdDay)
-
 forthDay.text(today.add(1, 'days').format("ddd MMMM Do"));
-console.log(forthDay)
-
 fifthDay.text(today.add(1, 'days').format("ddd MMMM Do"));
-console.log(fifthDay)
 
-// Store the information after submit
-submitBtn.click(store(startAddress, endAddress, cityName, date  ));
+
 
 
 
 //Stores items in localStorage
-function store(startAddress, endAddress, cityName, date  ) {
+function store() {
     var StartAddress = document.getElementById('from-input').value;
     var EndAddress = document.getElementById('too-input').value;
     var CityName = document.getElementById('destCity').value;
-    var Date = document.getElementById('date-picker').value;
+    
     
 var tripdata = []
 
@@ -58,7 +46,7 @@ $('#retrieve-data').click(retrievetripdata)
 //pull from localStorage
 function retrievetripdata(){
     var retrievekey = document.getElementById('retrievekey').value;
-    console.log("Retrieve Trip Data")
+    //console.log("Retrieve Trip Data")
 }
 
 //Loads page before functions execute
